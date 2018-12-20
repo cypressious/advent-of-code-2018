0e9c85588913d9fe75d4d1879f2dfd2976a06cb9
Kirill Rakhman <kirill.rakhman@gmail.com>
2018-12-20 14:00:56 +0100

    day 9 pt2

--- src/main/kotlin/day9.kt
+++ src/main/kotlin/day9.kt
@@ -1,50 +1,65 @@
 package day9
 
 fun main() {
-    play(9, 25, true)
+    play(9, 25)
     play(10, 1618)
     play(13, 7999)
     play(17, 1104)
     play(21, 6111)
     play(30, 5807)
     play(432, 71019)
+    play(432, 71019 * 100)
 }
 
-private fun play(countPlayers: Int, lastMarble: Int, print: Boolean = false) {
-    val marbles = mutableListOf(0)
-    val scores = IntArray(countPlayers)
-    var currentIndex = 0
+private fun play(countPlayers: Int, lastMarble: Int) {
+    val scores = LongArray(countPlayers)
+    var current = Node(0).also {
+        it.next = it
+        it.previous = it
+    }
 
     for (value in 1..lastMarble) {
-        val player = (value - 1) % countPlayers
-
         if (value % 23 != 0) {
-            currentIndex = (currentIndex + 2) % marbles.size
-
-            if (currentIndex == 0) currentIndex = marbles.size
-
-            marbles.add(currentIndex, value)
+            current.next(1).insertAfter(Node(value).also { current = it })
         } else {
-            scores[player] += value
+            val removed = current.next(-7).remove()
+            current = removed.next
 
-            currentIndex = Math.floorMod(currentIndex - 7, marbles.size)
-            scores[player] += marbles.removeAt(currentIndex)
+            val player = (value - 1) % countPlayers
+
+            scores[player] += value.toLong()
+            scores[player] += removed.value.toLong()
         }
 
-        if (print) {
-            val marblesString = buildString {
-                for (i in marbles.indices) {
-                    if (i != 0) append(" ")
-
-                    if (i == currentIndex) append("(")
-                    append(marbles[i])
-                    if (i == currentIndex) append(")")
-                }
-            }
-
-            println("[${player + 1}] $marblesString")
-        }
     }
 
     println(scores.max()!!)
+}
+
+private class Node(val value: Int) {
+    lateinit var next: Node
+    lateinit var previous: Node
+
+    fun insertAfter(node: Node) {
+        node.previous = this
+        next.previous = node
+
+        node.next = next
+        next = node
+    }
+
+    fun remove() = apply {
+        previous.next = next
+        next.previous = previous
+    }
+
+    fun next(i: Int): Node {
+        var result = this
+
+        repeat(Math.abs(i)) {
+            result = if (i > 0) result.next else result.previous
+        }
+
+        return result
+    }
 }
